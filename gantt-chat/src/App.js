0663fe4f922492010278378d405624de507619c5
PreetS123 <preetbhardwaj517@gmail.com>
2022-12-30 09:24:55 +0530

    stactic-bar-graph-done

--- gantt-chat/src/App.js
+++ gantt-chat/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import { GanntCharts } from './components/GanntCharts';
+import {Routes,Route} from 'react-router-dom';
+import { BarGraph } from './components/BarGraph';
 
 function App() {
   return (
     <div className="App">
        Hello world
+      <Routes>
+        <Route path='/' element={<BarGraph/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ gantt-chat/src/components/GanntCharts.js
@@ -0,0 +1,16 @@
+import React from 'react'
+// import { GanttComponent } from '@syncfusion/ej2-react-gantt';
+import {projectData} from '../data';
+
+export const GanntCharts = () => {
+  return (
+    <div>
+       {/* <GanttComponent
+       dataSource={projectData}
+
+       >
+
+       </GanttComponent> */}
+    </div>
+  )
+}
